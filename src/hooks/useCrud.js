9351fd6c0ddbe61f9465be3d7cbfa5cb281f4266
Richard Ailton Alfaro Manzano <richardalfaromanznao@gmail.com>
2022-12-15 09:51:14 -0500

    't'

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -4,14 +4,14 @@ import { useState } from 'react'
 const useCrud = () => {
     const [users, setUsers] = useState() //es para almacenar la data
     const getAllUsers = () =>{
-        const URL = 'http://users-crud.academlo.tech/users/'
+        const URL = 'https://users-crud.academlo.tech/users/'
         axios.get(URL)
         .then(res => setUsers(res.data))//dentro de data se tiene la información como tal
         .catch(err => console.log(err))
       }
     
       const createNewUser = data =>{
-        const URL = 'http://users-crud.academlo.tech/users/'
+        const URL = 'https://users-crud.academlo.tech/users/'
         axios.post(URL, data)
          .then(() =>{ getAllUsers()
                       
@@ -20,7 +20,7 @@ const useCrud = () => {
          .catch(err => console.log(err))
       }
       const deleteUserById = (id,data) =>{
-        const URL = `http://users-crud.academlo.tech/users/${id}/`
+        const URL = `https://users-crud.academlo.tech/users/${id}/`
         axios.delete(URL)
         .then(()=> {getAllUsers()
         })
@@ -28,7 +28,7 @@ const useCrud = () => {
       }
       
       const updateUserById = (id,data) => {
-        const URL = `http://users-crud.academlo.tech/users/${id}/`
+        const URL = `https://users-crud.academlo.tech/users/${id}/`
         axios.put(URL,data)//PARA QUE SE ACTUALICE
         .then(res=> getAllUsers())//PARA QUE SE VEA REFLEJADO EN LA VISTA
         .catch(err => console.log(err))
